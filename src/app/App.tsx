063a325673b66ae695e51fa390100640053041c2
spacey <3354909082@qq.com>
2023-04-07 20:28:34 +0800

    undo button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,10 @@ import { Game } from "../components/Game";
 import { Button } from "../components/Button";
 
 function App() {
-  const [date, setDate] = React.useState<Date>(new Date());
-
   return (
     <div className="App">
       <Button></Button>
-      <Game key={date.toISOString()} />
+      <Game />
     </div>
   );
 }
